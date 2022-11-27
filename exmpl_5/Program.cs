95ff993787779e7772bfa4fd2cdbab8d76ca0341
ANDREY PINK <andreypink@bk.ru>
2022-11-27 15:51:12 +0300

    add exmpl_5

---
+++ exmpl_5/Program.cs
@@ -0,0 +1,12 @@
+﻿// 'Hello' with 'if' and 'else'
+Console.Write("Enter Username ");
+string? username = Console.ReadLine();
+if(username.ToLower() == "masha") //ToLower() - convert username to lower font
+{
+    Console.WriteLine("Hello, This Masha!!");
+}
+else
+{
+    Console.Write("Hello,");
+    Console.WriteLine(username);
+}
