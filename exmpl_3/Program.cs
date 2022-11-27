3744ae943d1f4a8925e41c036da5aa1f547fb54f
ANDREY PINK <andreypink@bk.ru>
2022-11-27 14:45:16 +0300

    add exmpl_3

---
+++ exmpl_3/Program.cs
@@ -0,0 +1,11 @@
+﻿// summ 2 numbers
+int numberA = 3;
+int numberB = 5;
+Console.WriteLine(numberA + numberB);
+
+//else
+int numberC = 3;
+int numberD = 5;
+int result = numberC + numberD;
+Console.Write(result);
+
