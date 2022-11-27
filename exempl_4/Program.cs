1201edda257eb841fbc0cbeb22f5689eb64f8e46
ANDREY PINK <andreypink@bk.ru>
2022-11-27 14:54:43 +0300

    add exempl_4

---
+++ exempl_4/Program.cs
@@ -0,0 +1,8 @@
+﻿// sum random 2 numbers
+int numberA = new Random().Next(1,10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.Write(result);
+
