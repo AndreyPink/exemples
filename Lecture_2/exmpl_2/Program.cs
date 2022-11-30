8f3f3e0386368763441cff8ade530ab9bd2aabbc
ANDREY PINK <andreypink@bk.ru>
2022-11-30 20:04:32 +0300

    add exmpl_2

---
+++ Lecture_2/exmpl_2/Program.cs
@@ -0,0 +1,19 @@
+﻿//Нахождение максимально числа 
+//из 9-ти указанных через массив
+using System;
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int[] array = {12,23,23,46,34,57,68,12,13};
+
+int max = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8]));
+
+Console.WriteLine(max);
