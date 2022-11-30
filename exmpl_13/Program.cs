a2c94a4720d3b3c362ccde2add49e8aa93e5891d
ANDREY PINK <andreypink@bk.ru>
2022-11-30 19:42:25 +0300

    add exmpl_13

---
+++ exmpl_13/Program.cs
@@ -0,0 +1,23 @@
+﻿//Лекция_2
+//Задача_1 
+//Нахождение максимально числа из 9-ти указанных
+
+using System;
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int a1 = 3, b1 = 4, c1 = 6,
+a2 = 5, b2 = 12, c2 = 20,
+a3 = 15, b3 = 9, c3 = 10;
+
+int max = Max(
+    Max(a1, b1, c1),
+    Max(a2, b2, c2),
+    Max(a3, b3, c3));
+
+Console.WriteLine(max);
