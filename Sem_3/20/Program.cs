c88ae2118deddc1b74fd091ef835f91302aa29b2
ANDREY PINK <andreypink@bk.ru>
2022-12-06 11:13:45 +0300

    add new seminar

---
+++ Sem_3/20/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите координату Х1");
+int x1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите координату Y1");
+int y1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите координату Х2");
+int x2 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите координату Y2");
+int y2 = int.Parse(Console.ReadLine()!);
+
+double d = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+Console.WriteLine($"расстояние между точками равно {d}");
+
