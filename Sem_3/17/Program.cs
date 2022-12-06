bc92a190539498b64402c903a83157c500643b03
ANDREY PINK <andreypink@bk.ru>
2022-12-06 10:55:46 +0300

    add new seminar

---
+++ Sem_3/17/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача №17. Напишите программу, которая принимает
+//  на вход координаты точки (X и Y), 
+// причем X ≠ 0 и Y ≠ 0 и выдаёт номер 
+// четверти плоскости, в которой находится эта точка.
+
+Console.WriteLine("Введите координату Х");
+int x = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите координату Y");
+int y = int.Parse(Console.ReadLine()!);
+
+if(x > 0 && y > 0) {Console.WriteLine("1 четверть");}
+if(x > 0 && y < 0) {Console.WriteLine("4 четверть");}
+if(x < 0 && y < 0) {Console.WriteLine("3 четверть");}
+if(x < 0 && y > 0) {Console.WriteLine("2 четверть");}
---
+++ Sem_3/18/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, 
+// которая по заданному номеру четверти,
+//  показывает диапазон возможных координат
+//   точек в этой четверти (x и y).
+
+Console.WriteLine("Введите номер четверти");
+int x = int.Parse(Console.ReadLine()!);
+if(x == 1) Console.WriteLine("X = (0 : unlim), Y = (0 : unlim)");
+if(x == 2) Console.WriteLine("X = (0 : - unlim), Y = (0 : unlim)");
+if(x == 3) Console.WriteLine("X = (0 : - unlim), Y = (0 : - unlim)");
+if(x == 1) Console.WriteLine("X = (0 : unlim), Y = (0 : - unlim)");
