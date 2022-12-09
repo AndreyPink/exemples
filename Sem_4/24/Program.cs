61a58181a77555a4ca03e326303ad714b46c1d0a
ANDREY PINK <andreypink@bk.ru>
2022-12-09 10:45:28 +0300

    add New Sem

---
+++ Sem_4/24/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 24: Напишите программу, 
+// которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+// Console.WriteLine("Введите число N");
+// int n = int.Parse(Console.ReadLine()!);
+// int sum = 0;
+// for(int i = 1; i <= n; i++) sum+=i;
+// Console.Write($"Сумма чисел равна {sum}");
+
+Console.WriteLine("Введите число N");
+int n = int.Parse(Console.ReadLine()!);
+int GetSumNum(int num)
+{
+    int sum = 0;
+    while (num > 0)
+    {
+        sum += num;
+        num--;
+    }
+    return sum;
+}
+Console.Write($"Сумма чисел равна {GetSumNum(n)}");
---
+++ Sem_4/26/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 26: Напишите программу, которая принимает
+//  на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+
+int count = 0;
+int CountNum(int num)
+{
+    while (num > 0)
+    {
+        num /= 10;
+        count++;
+    }
+    return count;
+}
+
+Console.WriteLine("Введите число N");
+int n = int.Parse(Console.ReadLine()!);
+Console.Write($"Кол-во цифр в числе {n} = {CountNum(n)}");
+
