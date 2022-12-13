56b052a975d1edcf5c9f93e0844533d58a0b548d
ANDREY PINK <andreypink@bk.ru>
2022-12-13 13:49:20 +0300

    add SEM_5

---
+++ Sem_5/33/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 33: Задайте массив. Напишите программу, которая определяет, 
+// присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, 3] -> да
+
+int[] CreateArray()
+{
+    int[] array = new int[5];
+    for(int i = 0; i < 5; i++)
+    {
+        array[i] = new Random().Next(1, 1000);
+        Console.Write($"{array[i]} "); 
+    }
+    Console.WriteLine("\n");
+    return array;
+}
+
+void FindNum(int[] Array, int Num)
+{
+bool find = false;
+for(int i = 0; i <5; i++)
+{
+    if(Array[i] == Num) 
+    {
+        Console.WriteLine("Да");
+        find = true;
+        break;
+    }
+}
+if(find == false) Console.WriteLine("Нет");
+}
+Console.WriteLine("Введите число:");
+int num = Math.Abs(int.Parse(Console.ReadLine()!));
+FindNum(CreateArray(), num);
---
+++ Sem_5/35/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 35: Задайте одномерный массив из 123 случайных чисел. 
+// Найдите количество элементов массива, значения которых лежат в отрезке [10,99]. 
+// Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+
+int[] CreateArray()
+{
+    int[] array = new int[123];
+    for(int i = 0; i < 123; i++)
+    {
+        array[i] = new Random().Next(1, 1000);
+        Console.Write($"{array[i]} "); 
+    }
+    Console.WriteLine("\n");
+    return array;
+}
+
+void FindNum(int[] Array)
+{
+int sum = 0;
+for(int i = 0; i <123; i++)
+{
+    if(Array[i] >= 10 && Array[i] <= 99) sum++;
+}
+Console.WriteLine(sum);
+}
+FindNum(CreateArray());
+
