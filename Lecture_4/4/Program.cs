e2ad9ab56bbc3d14781a108b1dbeaa898a740d97
ANDREY PINK <andreypink@bk.ru>
2022-12-15 01:24:22 +0300

    add Lecture_4

---
+++ Lecture_4/4/Program.cs
@@ -0,0 +1,14 @@
+﻿
+// числа фибоначи
+
+double Fibonacci(int n)
+{
+    if (n == 1 || n == 2) return 1;
+    else return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+
+
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"{i} fib = {Fibonacci(i)}");
+}
