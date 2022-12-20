2b46e12c1717e16e14885f68184341bba5a5fe68
ANDREY PINK <andreypink@bk.ru>
2022-12-20 11:49:12 +0300

    add Sem_7

---
+++ Sem_7/46/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 46: Задайте двумерный массив размером m×n, 
+// заполненный случайными целыми числами.
+// m = 3, n = 4.
+// 1 4 8 19
+// 5 -2 33 -2
+// 77 3 8 1
+
+int[,] CreateArray(int a, int b)
+{
+int[,] matrix = new int[a, b];
+for (int rows = 0; rows < matrix.GetLength(0); rows++)
+{
+    for (int columns = 0; columns < matrix.GetLength(1); columns++)
+    {
+        matrix[rows, columns] = new Random().Next(-10,10);
+        Console.Write($"{matrix[rows, columns]} ");
+    }
+    Console.WriteLine("");
+}
+return matrix;
+}
+Console.WriteLine("Введите размер массива M: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите размер массива N: ");
+int n = int.Parse(Console.ReadLine()!);
+CreateArray(m,n);
+Console.WriteLine("");
+
---
+++ Sem_7/48/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 48. Задайте двумерный массив размером m×n, 
+// каждый элемент в массиве находится по формуле: A = m+n. Выведете полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+void CreateMatrix(int a, int b)
+{
+int[,] matrix = new int[a, b];
+for (int rows = 0; rows < matrix.GetLength(0); rows++)
+{
+    for (int columns = 0; columns < matrix.GetLength(1); columns++)
+    {
+        matrix[rows, columns] = rows + columns;;
+        Console.Write($"{matrix[rows, columns]} ");
+    }
+    Console.WriteLine("");
+}
+Console.WriteLine("");
+}
+CreateMatrix(3,4);
+
