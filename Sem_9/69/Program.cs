d6f3589fcdac91c6e61adc83e2910960ca2df547
ANDREY PINK <andreypink@bk.ru>
2022-12-24 23:36:45 +0300

    add Sem_9

--- Sem_9/69/Program.cs
+++ Sem_9/69/Program.cs
@@ -2,18 +2,17 @@
 // принимает два числа A и B и возводит число A в целую степень B
 // с помощью рекурсии.
 
-//МЕТОД ЧЕРЕЗ INT
-int DegreeNumber(int a, int b)
-{
-    if (b > 1) return a * DegreeNumber(a, b - 1);
-    else if (b == 0) return 1;
-    return a;
-}
-Console.WriteLine(DegreeNumber(2, 3));
-
 
 //МЕТОД ЧЕРЕЗ INT С ТЕРНАРНЫМ ОПЕРАТОРОМ
 int DegreeNumber2(int a, int b) 
 {return b == 0 ? 1 : a * DegreeNumber2(a, b - 1);}
 
 Console.WriteLine(DegreeNumber2(2, 10));
+
+//МЕТОД ЧЕРЕЗ INT
+int DegreeNumber(int a, int b)
+{
+    if (b == 0) return 1;
+    else return a * DegreeNumber(a, b - 1);
+}
+Console.WriteLine(DegreeNumber(2, 3));
